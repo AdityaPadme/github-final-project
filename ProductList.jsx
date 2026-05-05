3976f8fa5b238c35e89b5b0b408c062afd430489
AdityaPadme <aditya.padme23@vit.edu>
2026-05-06 01:08:58 +0530

    ProductList

---
+++ ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProductList() {
+    return (
+        <div>
+            <h2>Product List Coming Soon!</h2>
+        </div>
+    );
+}
+
+export default ProductList;
