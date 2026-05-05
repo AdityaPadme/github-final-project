771d4fa41526829a0cb763a78cad198346f9affb
AdityaPadme <aditya.padme23@vit.edu>
2026-05-06 01:03:45 +0530

    AboutUs

---
+++ AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <p>
+        Welcome to Paradise Nursery, where green meets serenity! 
+        We are passionate about bringing the beauty of nature into your home. 
+        Whether you are looking for air-purifying plants, aromatic herbs, or beautiful flowering plants, we have something for every plant lover.
+      </p>
+      <p>
+        Explore our collection and find the perfect green companion to brighten up your living space.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
